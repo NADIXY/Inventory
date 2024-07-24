4e388636d9a1a32a0fdb3282a4c1f55666aa6201
NADIXY <nadiagbaptista015@gmail.com>
2024-07-24 22:24:48 +0200

    Enum images

--- Inventory/Inventory/ImageAsset.swift
+++ Inventory/Inventory/ImageAsset.swift
@@ -18,6 +18,26 @@ enum ImageAsset: String, CaseIterable {
     case image8 = "stick 1"
     case image9 = "cd"
     case image10 = "festplatte"
+    case image11 = "smartphone"
+    case image12 = "smartphone2"
+    case image13 = "smartphone3"
+    case image14 = "smartwatch1"
+    case image15 = "staubsauger"
+    case image16 = "tv"
+    case image17 = "waschmaschine"
+    case image18 = "mikrowelle"
+    case image19 = "micro"
+    case image20 = "kühlschränke"
+    case image21 = "handy1"
+    case image22 = "gitarre"
+    case image23 = "drum"
+    case image24 = "betten"
+   
+    
+    
+
+    
+    
     
     var image: Image {
         Image(rawValue)
